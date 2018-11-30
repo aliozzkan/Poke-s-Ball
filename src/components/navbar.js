73b1abdc9fead6fd45bc7b03507e0440bfcf093f
Ali Özkan <34611025+aliozzkan@users.noreply.github.com>
2018-11-30 15:09:32 +0200

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
      return(
           <nav className="nav-wrapper red darken-1">
                <div className="container">
-                    <Link to="/" tag="a"  className="brand-logo">Poke' Times</Link>
+                    <Link to="/" tag="a"  className="brand-logo">Poke  Times</Link>
                     <ul className="right">
                          <li><NavLink to="/">Home</NavLink></li>
                          <li><NavLink to="/about">About</NavLink></li>
